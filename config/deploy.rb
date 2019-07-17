8ea511b9262dbe7dc0452fe76d025385a7692828
tipsypastels <dakotasankey@gmail.com>
2019-07-16 23:44:58 -0700

    debug log level

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,22 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 3
 
+set :log_level, :debug
+
 # Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
 # This is useful if you don't want to use ENV variables
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
 set :default_run_options, { pty: true }
+
+before "deploy:assets:precompile", "deploy:yarn_install"
+
+namespace :deploy do
+  desc 'Run rake yarn:install'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install")
+      end
+    end
+  end
+end
